da1e8e6fcad2a5c1fdaafa80c7c5a3fb2edc9a37
CertAndKey <38504609+CertAndKey@users.noreply.github.com>
2023-09-28 14:35:12 -0400

    Add files via upload

---
+++ GetIPsFromPCAP.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+if [ $# -eq 0 ];
+then
+  echo "please supply a file"
+  echo "EXAMPLE: ./GetIPsFromPCAP.sh file.txt"
+  exit 1
+elif [ $2 ];
+then
+  echo "Too many arguments: $@"
+  echo "EXAMPLE: ./GetIPsFromPCAP.sh file.txt"
+  exit 1
+fi
+
+tshark -r $1 -T fields -e ip.addr > ips.txt
+sort ips.txt | uniq > ips_no_dupe.txt
+sed -i 's/192.168.200.164//g' ips_no_dupe.txt
+#sed -i 's/<ip_to_filter>//g' ips_no_dupe.txt
+sed -i 's/,//g' ips_no_dupe.txt
+rm ips.txt
+sort ips_no_dupe.txt | uniq > ips.txt
+rm ips_no_dupe.txt
+cat ips.txt
+rm ips.txt
